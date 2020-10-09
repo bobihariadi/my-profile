5f37562841825da2600b96b0409dee6d7fecb5ff
bobihariadi <bobihariadi@gmial.com>
2020-10-09 14:11:07 +0700

    Updates

---
+++ precache-manifest.4e08eb8fb8249c463ea77f2021d4e464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74e05404680176fd1d384735322d0a0",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "ff1b7967c5d4cc2f7b4d",
+    "url": "/my-profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0db6a210e0d6421056d4",
+    "url": "/my-profile/static/js/2.92265079.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.92265079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1b7967c5d4cc2f7b4d",
+    "url": "/my-profile/static/js/main.44e2bef8.chunk.js"
+  },
+  {
+    "revision": "c1d853bd7e89e86f5c06",
+    "url": "/my-profile/static/js/runtime-main.58046206.js"
+  }
+]);
